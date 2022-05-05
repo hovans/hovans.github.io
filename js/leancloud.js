73ee7dd756833723e3b5a286acdfbf2b05370fda
hovans <hovnen20@gmail.com>
2022-05-05 21:41:00 +0800

    Site updated: 2022-05-05 21:40:59

--- js/leancloud.js
+++ js/leancloud.js
@@ -41,51 +41,52 @@
             }
         };
     }
-    var r = CONFIG.web_analytics.leancloud.app_id, i = CONFIG.web_analytics.leancloud.app_key, e = CONFIG.web_analytics.leancloud.server_url;
-    function t(o) {
-        !function (t) {
-            var i = !0 === CONFIG.web_analytics.enable, e = [], a = [], n = h.querySelector('#leancloud-site-pv-container');
-            if (n) {
-                var o = d(t, 'site-pv').then(function (e) {
-                    i && a.push(f(e.objectId));
-                    var t = h.querySelector('#leancloud-site-pv');
-                    t && (t.innerText = e.time + 1, n && (n.style.display = 'inline'));
-                });
-                e.push(o);
-            }
-            var c = h.querySelector('#leancloud-site-uv-container');
-            if (c) {
-                var r = d(t, 'site-uv').then(function (e) {
-                    var t, n, o = (t = 'LeanCloud_UV_Flag', !((n = localStorage.getItem(t)) && new Date().getTime() - parseInt(n, 10) <= 86400000 || (localStorage.setItem(t, new Date().getTime().toString()), 0)));
-                    o && i && a.push(f(e.objectId));
-                    var r = h.querySelector('#leancloud-site-uv');
-                    r && (r.innerText = e.time + (o ? 1 : 0), c && (c.style.display = 'inline'));
-                });
-                e.push(r);
-            }
-            var l = h.querySelector('#leancloud-page-views-container');
-            if (l) {
-                var s = decodeURI(p.location.pathname.replace(/(?<!\/)\/*(index.html)*$/, '/')), u = d(t, s).then(function (e) {
-                        if (i && a.push(f(e.objectId)), l) {
-                            var t = h.querySelector('#leancloud-page-views');
-                            t && (t.innerText = (e.time || 0) + 1, l.style.display = 'inline');
-                        }
-                    });
-                e.push(u);
-            }
-            i && Promise.all(e).then(function () {
-                var e, o;
-                0 < a.length && (e = t, o = a, new Promise(function (t, n) {
-                    e('post', '/batch', { requests: o }).then(function (e) {
-                        if ((e = e.json()).error)
-                            throw e.error;
-                        t(e);
-                    }).catch(function (e) {
-                        console.error('Failed to save visitor count', e), n(e);
-                    });
-                }));
+    function e(t) {
+        var i = !0 === CONFIG.web_analytics.enable, e = [], a = [], n = h.querySelector('#leancloud-site-pv-container');
+        if (n) {
+            var o = d(t, 'site-pv').then(function (e) {
+                i && a.push(f(e.objectId));
+                var t = h.querySelector('#leancloud-site-pv');
+                t && (t.innerText = e.time + 1, n && (n.style.display = 'inline'));
             });
-        }(function (e, t, n) {
+            e.push(o);
+        }
+        var c = h.querySelector('#leancloud-site-uv-container');
+        if (c) {
+            var r = d(t, 'site-uv').then(function (e) {
+                var t, n, o = (t = 'LeanCloud_UV_Flag', !((n = localStorage.getItem(t)) && new Date().getTime() - parseInt(n, 10) <= 86400000 || (localStorage.setItem(t, new Date().getTime().toString()), 0)));
+                o && i && a.push(f(e.objectId));
+                var r = h.querySelector('#leancloud-site-uv');
+                r && (r.innerText = e.time + (o ? 1 : 0), c && (c.style.display = 'inline'));
+            });
+            e.push(r);
+        }
+        var l = h.querySelector('#leancloud-page-views-container');
+        if (l) {
+            var s = decodeURI(p.location.pathname.replace(/(?<!\/)\/*(index.html)*$/, '/')), u = d(t, s).then(function (e) {
+                    if (i && a.push(f(e.objectId)), l) {
+                        var t = h.querySelector('#leancloud-page-views');
+                        t && (t.innerText = (e.time || 0) + 1, l.style.display = 'inline');
+                    }
+                });
+            e.push(u);
+        }
+        i && Promise.all(e).then(function () {
+            var e, o;
+            0 < a.length && (e = t, o = a, new Promise(function (t, n) {
+                e('post', '/batch', { requests: o }).then(function (e) {
+                    if ((e = e.json()).error)
+                        throw e.error;
+                    t(e);
+                }).catch(function (e) {
+                    console.error('Failed to save visitor count', e), n(e);
+                });
+            }));
+        });
+    }
+    var r = CONFIG.web_analytics.leancloud.app_id, i = CONFIG.web_analytics.leancloud.app_key, t = CONFIG.web_analytics.leancloud.server_url;
+    function n(o) {
+        e(function (e, t, n) {
             return fetch(o + '/1.1' + t, {
                 method: e,
                 headers: {
@@ -97,10 +98,10 @@
             });
         });
     }
-    var n = '-MdYXbMMI' !== r.slice(-9) ? e : 'https://' + r.slice(0, 8).toLowerCase() + '.api.lncldglobal.com';
-    n ? t(n) : fetch('https://app-router.leancloud.cn/2/route?appId=' + r).then(function (e) {
+    var o = '-MdYXbMMI' !== r.slice(-9) ? t : 'https://' + r.slice(0, 8).toLowerCase() + '.api.lncldglobal.com';
+    o ? n(o) : fetch('https://app-router.leancloud.cn/2/route?appId=' + r).then(function (e) {
         return e.json();
     }).then(function (e) {
-        e.api_server && t('https://' + e.api_server);
+        e.api_server && n('https://' + e.api_server);
     });
 }(window, document);
